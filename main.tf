033b018d79f1a9f7fe4abcf68626e7bc4891350e
Anderson <amurasaki@gmail.com>
2024-10-03 16:29:41 -0300

    terraform do rds aurora

--- main.tf
+++ main.tf
@@ -2,25 +2,23 @@ provider "aws" {
   region = "us-east-1"  # Altere para a região desejada
 }
 
-resource "aws_rds_cluster" "techchallengeautoatendimentodb" {
-  cluster_identifier  = "techchallengeautoatendimentodb-cluster"
+resource "aws_rds_cluster" "autoatendimentodb" {
+  cluster_identifier  = "autoatendimentodb-cluster"
   engine              = "aurora-postgresql"
   engine_version      = "15.2"  # Altere conforme necessário
   master_username     = var.db_username
   master_password     = var.db_password
   database_name       = var.db_name 
   skip_final_snapshot = true
-  role_arn            = var.lab_role
 }
 
-resource "aws_rds_cluster_instance" "techchallengeautoatendimentodb" {
+resource "aws_rds_cluster_instance" "autoatendimentodb" {
   count              = 1
-  cluster_identifier = aws_rds_cluster.techchallengeautoatendimentodb.id
+  cluster_identifier = aws_rds_cluster.autoatendimentodb.id
   instance_class     = "db.t3.small"  # Classe de instância mínima
-  engine             = aws_rds_cluster.techchallengeautoatendimentodb.engine
-  role_arn           = var.lab_role
+  engine             = aws_rds_cluster.autoatendimentodb.engine
 }
 
 output "db_endpoint" {
-  value = aws_rds_cluster.example.endpoint
+  value = aws_rds_cluster.autoatendimentodb.endpoint
 }
